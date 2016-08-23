06e5b47b8c69a11e8ff086df380e225da2295163
Luka Nadiradze <thrilled.space@gmail.com>
2016-08-23 23:10:30 +0400

    Player movement
    Early version :clock1:

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	private KeyCode left;
+	private KeyCode right;
+	private bool facingRight;
+	private SpriteRenderer playerSpriteRenderer;
+
+	void Start () {
+		left = KeyCode.A;
+		right = KeyCode.D;
+		facingRight = true;
+		playerSpriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	void FixedUpdate () {
+
+		if (facingRight) {
+			playerSpriteRenderer.flipX = false;
+		} else {
+			playerSpriteRenderer.flipX = true;
+		}
+
+		if (Input.GetKey (right)) {
+			facingRight = true;
+			transform.Translate(new Vector2(0.1f * speed, 0));
+		}
+		if (Input.GetKey(left)){
+			facingRight = false;
+			transform.Translate(new Vector2(-0.1f * speed, 0));
+		}
+	}
+}
---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneScript : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	
+	}
+}
