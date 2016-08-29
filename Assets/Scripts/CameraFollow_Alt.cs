4cafe097a273ee8e729989178c3df3602c821ad0
Luka Nadiradze <thrilled.space@gmail.com>
2016-08-29 23:30:45 +0400

    Started automatic plafrom generation
    But still :bug:s.

---
+++ Assets/Scripts/CameraFollow_Alt.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow_Alt: MonoBehaviour {
+
+	public Transform playerTransform;
+	[Range(-1f,1f)]
+	public float cameraOffsetX;
+
+	private float cameraHalfWidth;
+
+	void Start () {
+		cameraHalfWidth =  Camera.main.orthographicSize * Camera.main.aspect;
+	}
+	
+	void Update () {
+		if (ChangeOrNot ()) {
+			Vector2 futurePos = Vector2.Lerp (transform.position, playerTransform.position, 0.1f);
+			transform.position = new Vector3 (playerTransform.position.x - cameraHalfWidth * cameraOffsetX, futurePos.y, -10);
+		} else {
+			transform.position = new Vector3 (playerTransform.position.x - cameraHalfWidth * cameraOffsetX, transform.position.y, -10);
+		}
+	}
+
+	bool ChangeOrNot(){
+		if(PlayerMovement.grounded){
+			return true;
+		}
+		return false;
+	}
+}
--- Assets/Scripts/SceneScript.cs
+++ Assets/Scripts/SceneScript.cs
@@ -1,14 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class SceneScript : MonoBehaviour {
 
+	public List<GameObject> platformPrefabs;
+	public int timesX = 5;
+
+	private float prevBounds;
+	private float distanceBetween;
+	private float maxDistanceFromY;
+
 	void Start () {
-		
+		prevBounds = 0;
+		distanceBetween = 1;
+
+		LayoutPlatforms ();
 	}
-	
-	void Update () {
-	
+
+	void LayoutPlatforms(){
+		for (int t = 0; t < timesX; t++){
+			int randomIndex = Random.Range (0, platformPrefabs.Count);
+
+			GameObject instance = Instantiate (
+				platformPrefabs [randomIndex], 
+				new Vector2(
+					platformPrefabs[randomIndex].transform.position.x + prevBounds * 2 + distanceBetween,
+					platformPrefabs[randomIndex].transform.position.y + DistanceFromY()
+				),
+				Quaternion.identity) as GameObject;
+			
+			prevBounds = instance.GetComponent<SpriteRenderer> ().bounds.max.x;
+		}
+	}
+
+	float DistanceFromY(){
+		float distance = Random.Range (-5, 5);
+		return distance;
 	}
 }
