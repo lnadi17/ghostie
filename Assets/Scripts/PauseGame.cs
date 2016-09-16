0ccb48c64bd8030219a5f559ebddff73a20bb42d
Luka Nadiradze <thrilled.space@gmail.com>
2016-09-16 21:02:03 +0400

    Added sprites for pause menu
    Working on some bugs.

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -6,21 +6,17 @@ public class PauseGame : MonoBehaviour {
 
 	public GameObject pauseGameObject;
 
-	private PlayerMovement pMovement;
+	//private PlayerMovement pMovement;
 
 	void Start () {
-		pMovement = GetComponent<PlayerMovement> ();
+		//pMovement = GetComponent<PlayerMovement> ();
 	}
 
 	public void Pause(){
 		Time.timeScale = 0;
-
-		pMovement.enabled = false;
 	}
 
 	public void Resume(){
 		Time.timeScale = 1;
-
-		pMovement.enabled = true;
 	}
 }
